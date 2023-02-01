74e23c2f78c49bdd1a5dc4f41ead019c7cb4ea5d
Zodecok <118003544+Zodecok@users.noreply.github.com>
2023-02-01 18:00:27 +1000

    Introduce model TDD and the base files
    The model tests are created, and the README file has basic scaffolding
    that is to be changed when ready to release
    This was modified to have the completed winner methods scripter into the
    tests

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # odin_connect_4
+
+plan
+board is 7 by 6
+Testing just the model
+Model Methods
+    Method add_piece(column) throws OutOfBoundsError
+    Method won? 
+    Method game_over?
+    Method reset
+
+the board determines which go it is based off the number of pieces on the board
+The board is created through inbedded arrays
+It has checking methods that are used to check whether there are nodes connect
+The 
+black_obj = "\u26AB"
+black_obj = "\u26AA"
+
+
---
+++ spec/connect_4_rspec.rb
@@ -0,0 +1,158 @@
+require "./lib/connect_4.rb"
+
+describe ConnectFour do
+  describe "#initialize" do
+    it "has a board filled with underscores when created" do
+      connect = ConnectFour.new
+      expect(connect.board).to eql([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_]])
+      end
+    end
+
+    it "has the board being equal to the input when given a parameter" do
+      connect = ConnectFour.new([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :⚪, :_, :_, :_, :_]])
+      expect(connect.board).to eql([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :⚪, :_, :_, :_, :_]]) 
+    end
+
+  end
+
+  describe "#turn" do
+
+    before(:example) do
+      @connect = ConnectFour.new
+
+
+    xit "piece is added to board at correct location" do
+      @connect.turn(0)
+      expect(@connect.board).to eql([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :_, :_, :_, :_, :_]])
+    end
+
+    xit "piece changes to white for the second player's move" do
+      @connect.turn(0)
+      @connect.turn(2)
+      expect(@connect.board).to eql([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :⚪, :_, :_, :_, :_]])
+    end
+
+    xit "piece stacks on another if space below it is filled" do
+      @connect.turn(0)
+      @connect.turn(0)
+      expect(@connect.board).to eql([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚪, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :_, :_, :_, :_, :_]])
+    end
+
+    xit "throws error when column index is out of range" do
+      expect { @connect.turn(10) }.to raise_error(StandardError::OutOfBoundsError)
+    end
+
+    xit "throws error when column is too high" do
+      6.times { @connect.turn(1) }
+      expect { @connect.turn(1) }.to raise_error(StandardError::OutOfBoundsError)
+    end
+  end
+
+  describe "#game_over?" do
+    xit "returns false when the game isn't won" do
+      connect = ConnectFour.new
+      expect(connect.game_over?).to eql(false)
+    end
+
+    xit "returns true when a horizontal win" do
+      connect = ConnectFour.new([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :⚫, :_, :_, :_, :_],                                                                      
+        [:⚫, :⚪, :⚪, :⚪, :⚪, :_, :_]])
+      expect(connect.game_over?).to eql(true)
+    end
+
+    xit "returns true when a vertical win" do
+      connect = ConnectFour.new([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :_, :⚫, :⚪, :_, :_, :_],                                                                      
+        [:⚫, :⚪, :⚪, :⚪, :_, :_, :_]])
+      expect(connect.game_over?).to eql(true)
+    end
+
+    xit "returns true when a left diagonal win" do
+      connect = ConnectFour.new([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚪, :_, :_, :_, :_, :_, :_],                                                                      
+        [:⚫, :⚪, :_, :_, :_, :_, :_],                                                                      
+        [:⚪, :⚫, :⚪, :_, :_, :_, :_],                                                                      
+        [:⚫, :⚫, :⚫, :⚪, :_, :_, :_]])
+      expect(connect.game_over?).to eql(true)
+    end
+
+    xit "returns true when a right diagonal win" do
+      connect = ConnectFour.new([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :⚫],                                                                      
+        [:_, :_, :_, :_, :_, :⚫, :⚪],                                                                      
+        [:_, :_, :_, :_, :⚫, :⚪, :⚫],                                                                      
+        [:_, :_, :⚫, :⚫, :⚪, :⚪, :⚪]])
+      expect(connect.game_over?).to eql(true)
+    end
+  end
+
+  describe "winner" do
+    xit "returns nil when no winner" do 
+      connect = ConnectFour.new([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_]])
+      expect(connect.winner).to eql(nil)
+    end
+
+    xit "returns white token when player two wins"
+
+  describe "#reset" do
+    xit "resests board to original" do
+      connect = ConnectFour.new([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :⚫],                                                                      
+        [:_, :_, :_, :_, :_, :⚫, :⚪],                                                                      
+        [:_, :_, :_, :_, :⚫, :⚪, :⚫],                                                                      
+        [:_, :_, :⚫, :⚫, :⚪, :⚪, :⚪]])
+      connect.reset
+      expect(connect.board).to eql([[:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_],                                                                      
+        [:_, :_, :_, :_, :_, :_, :_]])
+    end
+  end
+end
